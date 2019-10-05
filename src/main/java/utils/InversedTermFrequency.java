ab4bb5942d32c2743abe017bc4514925148ef11e
RedMoon32 <rinat.babichev@gmail.com>
2019-10-05 15:21:40 +0300

    Itf Initial version

--- src/main/java/utils/InversedTermFrequency.java
+++ src/main/java/utils/InversedTermFrequency.java
@@ -1,9 +1,7 @@
 package utils;
 
-import java.io.IOException;
-import java.util.StringTokenizer;
-
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
@@ -16,26 +14,32 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.json.JSONObject;
 
-public class TermFrequency extends Configured implements Tool {
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+import java.lang.Math;
+
+public class InversedTermFrequency extends Configured implements Tool {
+
+    private final static String FILE_COUNT = "CountOfFiles";
 
     public static class TokenizerMapper
             extends Mapper<Object, Text, Text, DoubleWritable> {
 
-        private final  DoubleWritable one = new DoubleWritable(1.0);
+        private final DoubleWritable one = new DoubleWritable(1.0);
 
         public void map(Object key, Text document, Context context) throws IOException, InterruptedException {
             JSONObject json = new JSONObject(document.toString());
             Text content = new Text(json.get("text").toString());
-            String doc_id = json.get("id").toString();
             StringTokenizer words = new StringTokenizer(content.toString(), " \'\n.,!?:()[]{};\\/\"*");
-            int total_words_num = words.countTokens();
+            HashSet<String> procedeed = new HashSet<String>();
             while (words.hasMoreTokens()) {
                 String word = words.nextToken().toLowerCase();
-                if (word.equals("")) {
+                if (word.equals("") || procedeed.contains(word)) {
                     continue;
                 }
-                DoubleWritable freq = new DoubleWritable(one.get()/(double)total_words_num);
-                context.write(new Text("("+word+","+doc_id+")"), freq);
+                procedeed.add(word);
+                context.write(new Text(word), one);
             }
         }
     }
@@ -47,30 +51,37 @@ public class TermFrequency extends Configured implements Tool {
         public void reduce(Text key, Iterable<DoubleWritable> values,
                            Context context
         ) throws IOException, InterruptedException {
-            double sum = 0;
-            for (DoubleWritable val : values) {
-                sum += val.get();
+            double fileCount = Double.parseDouble(String.valueOf(context.getConfiguration().get(FILE_COUNT)));
+            int occuredDocCount = 0;
+            for (DoubleWritable v : values) {
+                occuredDocCount++;
             }
-            result.set(sum);
+            result.set(Math.log(fileCount/occuredDocCount));
             context.write(key, result);
         }
     }
 
+    public int getCountOfFiles() throws IOException {
+        FileSystem fs = FileSystem.get(getConf());
+        return fs.listStatus(new Path(Paths.INPUT_PATH)).length;
+    }
 
     public int run(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-        Job job = Job.getInstance(getConf(), "word count");
-        job.setJarByClass(TermFrequency.class);
+        Job job = Job.getInstance(getConf(), "idf");
+        job.setJarByClass(InversedTermFrequency.class);
         job.setMapperClass(TokenizerMapper.class);
         job.setReducerClass(Reduce.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(DoubleWritable.class);
         FileInputFormat.addInputPath(job, new Path(Paths.INPUT_PATH));
-        FileOutputFormat.setOutputPath(job, new Path(Paths.TF_OUT));
+        FileOutputFormat.setOutputPath(job, new Path(Paths.IDF_OUT));
+        Integer docCount = getCountOfFiles();
+        job.getConfiguration().set(FILE_COUNT, docCount.toString());
         return job.waitForCompletion(true) ? 0 : 1;
     }
 
     public static void main(String[] args) throws Exception {
-        int resultOfJob = ToolRunner.run(new TermFrequency(),args);
+        int resultOfJob = ToolRunner.run(new InversedTermFrequency(), args);
         System.exit(resultOfJob);
     }
 }
